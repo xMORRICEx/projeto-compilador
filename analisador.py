4815c5bc9d78d6027e11f3f2347241f8f8261079
xMORRICEx <morrice016@outlook.com>
2023-05-26 14:40:41 -0300

    my last update

--- analisador.py
+++ analisador.py
@@ -9,6 +9,9 @@ tokens = (
     'ATRIBUICAO',
 )
 
+# Tabela de símbolos
+tabela_simbolos = {}
+
 # Regras de expressão regular para tokens simples
 t_IDENTIFICADOR = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_OPERADOR = r'[-+*/]'
@@ -36,6 +39,10 @@ precedence = (
     ('left', 'OPERADOR'),
 )
 
+# Variáveis globais para rastrear erros
+erro_encontrado = False
+erro_msg = ""
+
 # Regra para expressão
 def p_expression(p):
     '''
@@ -44,37 +51,74 @@ def p_expression(p):
                | IDENTIFICADOR
                | NUMERO
     '''
-    if len(p) == 4:
-        if p[2] == '=':
-            p[0] = p[3]
-            print(f"{p[1]} = {p[0]}")
+    global erro_encontrado, erro_msg
+
+    if len(p) == 4 and p[2] == '=':
+        if isinstance(p[3], tuple):
+            # Verificar erros de mais de três operandos ou operadores após o símbolo de atribuição
+            if len(p[3]) > 3:
+                erro_encontrado = True
+                erro_msg = "Erro: Foram encontrados mais de três operandos ou operadores após o símbolo de atribuição"
+            else:
+                p[0] = p[3]
+                tabela_simbolos[p[1]] = p[0]
         else:
-            print("Erro: Após o símbolo de atribuição não pode aceitar mais de três operandos ou operadores")
-            p[0] = None
+            p[0] = p[3]
+            tabela_simbolos[p[1]] = p[0]
     else:
         p[0] = p[1]
 
+        # Verificar erros de operandos e operadores consecutivos
+        if isinstance(p[0], str) and len(p[0]) > 1:
+            erro_encontrado = True
+            erro_msg = "Erro: Dois operandos consecutivos encontrados separados por espaços em branco"
+
+        if isinstance(p[0], str) and p[0] in ['+', '-', '*', '/']:
+            erro_encontrado = True
+            erro_msg = "Erro: Dois operadores consecutivos encontrados separados por espaços em branco"
+
 # Tratamento de erros de sintaxe
 def p_error(p):
+    global erro_encontrado, erro_msg
+
     if p:
-        print("Erro de sintaxe: '%s'" % p.value)
+        erro_encontrado = True
+        erro_msg = f"Erro de sintaxe: '{p.value}'"
     else:
-        print("Erro de sintaxe: fim de arquivo inesperado")
+        erro_encontrado = True
+        erro_msg = "Erro de sintaxe: fim de arquivo inesperado"
 
 # Construção do parser
 parser = yacc.yacc()
 
 # Função para analisar a entrada
 def analisar_entrada(entrada):
+    global erro_encontrado, erro_msg
+
+    # Resetar variáveis de erro
+    erro_encontrado = False
+    erro_msg = ""
+
+    tabela_simbolos.clear()
+
     lexer.input(entrada)
     while True:
         token = lexer.token()
         if not token:
             break
         print(f"{token.value} : {token.type}")
+
     parser.parse(entrada)
 
-# Loop para entrada de dados
+    if not erro_encontrado:
+        print("Análise léxica concluída sem erros.")
+        print("Tabela de Símbolos:")
+        for chave, valor in tabela_simbolos.items():
+            print(f"{chave} : {valor}")
+    else:
+        print(erro_msg)
+
+# Loop principal para entrada de dados
 while True:
     entrada = input("Entrada de dados (digite 'sair' para encerrar): ")
     if entrada == 'sair':
